fa177b6abe6ad5ac9f68ee0c021225bdde8674ce
Ryan Current <rcurrent37@gmail.com>
2023-01-30 17:42:39 -0500

    New: updated adjustment calculator

--- Android/app/src/main/java/com/rcdevelopment/phsensor/CalculateAdjustment.java
+++ Android/app/src/main/java/com/rcdevelopment/phsensor/CalculateAdjustment.java
@@ -4,27 +4,38 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.util.ArrayList;
 
 public class CalculateAdjustment extends Fragment {
 
-    private TextView adjustmentValueTextView;
+    private TextView mTextView, bTextView;
     private EditText knownPH1EditText, knownPH2EditText, knownPH3EditText,
                      rawValue1EditText, rawValue2EditText, rawValue3EditText;
     private Button readRaw1Button, readRaw2Button, readRaw3Button, saveAdjustmentValueButton;
+    private Double m, b;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_calculate_adjustment, container, false);
 
-        this.adjustmentValueTextView = view.findViewById(R.id.adjustmentValueTextView);
+        this.mTextView = view.findViewById(R.id.adjustmentMTextView);
+        this.bTextView = view.findViewById(R.id.adjustmentBTextView);
         this.saveAdjustmentValueButton = view.findViewById(R.id.saveAdjustmentValueButton);
+        m = 0.0;
+        b = 0.0;
 
         this.knownPH1EditText = view.findViewById(R.id.knownPH1EditText);
         this.knownPH2EditText = view.findViewById(R.id.knownPH2EditText);
@@ -41,7 +52,109 @@ public class CalculateAdjustment extends Fragment {
         this.saveAdjustmentValueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                CalculateAdjustment.this.saveAdjustmentValue();
+                CalculateAdjustment.this.saveAdjustmentValues();
+            }
+        });
+
+        this.knownPH1EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                CalculateAdjustment.this.calculateLinearRegression();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.knownPH2EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                CalculateAdjustment.this.calculateLinearRegression();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.knownPH3EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                CalculateAdjustment.this.calculateLinearRegression();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.rawValue1EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                CalculateAdjustment.this.calculateLinearRegression();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.rawValue2EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                CalculateAdjustment.this.calculateLinearRegression();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        this.rawValue3EditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                CalculateAdjustment.this.calculateLinearRegression();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
             }
         });
 
@@ -71,7 +184,7 @@ public class CalculateAdjustment extends Fragment {
     }
 
 
-    private void saveAdjustmentValue() {
+    private void saveAdjustmentValues() {
 
     }
 
@@ -91,4 +204,93 @@ public class CalculateAdjustment extends Fragment {
     }
 
 
+    private void setM(Double m) {
+        this.m = m;
+        this.mTextView.setText("m = " + m.toString());
+    }
+
+
+    private void setB(Double b) {
+        this.b = b;
+        this.bTextView.setText("b = " + b.toString());
+    }
+
+
+    private void calculateLinearRegression() {
+        ArrayList<Double> phValues = new ArrayList<>();
+        ArrayList<Double> rawValues = new ArrayList<>();
+
+        try {
+            phValues.add(Double.parseDouble(knownPH1EditText.getText().toString()));
+        } catch (NumberFormatException exception) {
+            Toast.makeText(getContext(), "Cannot parse pH value 1", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            phValues.add(Double.parseDouble(knownPH2EditText.getText().toString()));
+        } catch (NumberFormatException exception) {
+            Toast.makeText(getContext(), "Cannot parse pH value 2", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            phValues.add(Double.parseDouble(knownPH3EditText.getText().toString()));
+        } catch (NumberFormatException exception) {
+            // do nothing because the 3rd value is not required
+        }
+
+        try {
+            rawValues.add(Double.parseDouble(rawValue1EditText.getText().toString()));
+        } catch (NumberFormatException exception) {
+            Toast.makeText(getContext(), "Cannot parse raw value 1", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            rawValues.add(Double.parseDouble(rawValue2EditText.getText().toString()));
+        } catch (NumberFormatException exception) {
+            Toast.makeText(getContext(), "Cannot parse raw value 2", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        try {
+            rawValues.add(Double.parseDouble(rawValue3EditText.getText().toString()));
+        } catch (NumberFormatException exception) {
+            // do nothing because the 3rd value is not required
+        }
+
+        if(phValues.size() != rawValues.size()) {
+            Toast.makeText(getContext(), "Not all rows are fully filled out", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        LinearRegression linearRegression = new LinearRegression(phValues, rawValues);
+
+        this.setM(linearRegression.GetM());
+        this.setB(linearRegression.GetB());
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Android/app/src/main/java/com/rcdevelopment/phsensor/LinearRegression.java
+++ Android/app/src/main/java/com/rcdevelopment/phsensor/LinearRegression.java
@@ -12,9 +12,51 @@ public class LinearRegression {
     private ArrayList<Double> yValues;
 
 
-    public LinearRegression(ArrayList<Double> xValues, ArrayList<Double> yValues) {
-        
+    public LinearRegression(ArrayList<Double> xValues, ArrayList<Double> yValues) throws IllegalArgumentException {
+        this.xValues = xValues;
+        this.yValues = yValues;
+        if(xValues.size() != yValues.size())
+            throw new IllegalArgumentException("xValues and yValues must have the same size.");
+        this.calcLinearRegression();
     }
+
+
+    public double GetM() {
+        return this.m;
+    }
+
+
+    public double GetB() {
+        return this.b;
+    }
+
+
+    private void calcLinearRegression() {
+        int n = xValues.size();
+
+        double sumX = 0.0;
+        double sumY = 0.0;
+        double sumXY = 0.0;
+        double sumXSquared = 0.0;
+
+        for(int i = 0; i < n; i++) {
+            double xValue = xValues.get(i);
+            double yValue = yValues.get(i);
+            sumX += xValue;
+            sumY += yValue;
+            sumXY += xValue * yValue;
+            sumXSquared += xValue * xValue;
+        }
+
+        // least squares formulas for m and b
+        m = ((n * sumXY) - (sumX * sumY)) / ((n * sumXSquared) - (sumX * sumX)));
+
+        b = (((sumY) - (m * sumX)) / n);
+
+    }
+
+
+
 
 
 }
